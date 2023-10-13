ca0040d353a0be1105014ddbfc4d5e4b2d4c1eb3
herrtane <herrtane@gmail.com>
2023-10-13 23:00:56 +0900

    Update README.MD

--- src/FTPInspectModule.py
+++ src/FTPInspectModule.py
@@ -233,7 +233,7 @@ def geofenceInspect():
     ###########################################################
     # Windows의 경우 아래의 주석을 해제
     # parser_fd = os.open("./fs/microsd/dataman", os.O_BINARY)
-    
+
     # Windows의 경우 아래의 코드 한줄을 주석처리
     parser_fd = os.open("./fs/microsd/dataman", os.O_RDONLY)
     ###########################################################
